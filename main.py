8f1d4a37375243c68adb14873345b3be03a55cba
Phil Hord <phil.hord@gmail.com>
2019-12-12 02:49:52 +0000

    Use math instead of strings, remove some duplicate code, replace wordlist join with `join`, use lists instead of dictionaries, remove special handling for "teens"

--- main.py
+++ main.py
@@ -3,73 +3,71 @@
 
 def sub1000(i):
     '''Spells out any integer 0-999'''
-    #Create dictionaries to translate number:word depending on its place
-    onesdict = dict({1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'})
-    tensdict = dict({2:'twenty',3:'thirty',4:'forty',5:'fifty',6:'sixty',7:'seventy',8:'eighty',9:'ninety'})
-    teensdict = dict({0:'ten',1:'eleven',2:'twelve',3:'thirteen',4:'fourteen',5:'fifteen',6:'sixteen',7:'seventeen',8:'eighteen',9:'nineteen'})
+
+    #Create lookups to translate number:word depending on its place
+    oneslist = ['zero','one','two','three','four','five','six','seven','eight','nine',
+                'ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+    tenslist = [None, None,'twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+
     #Convert integer to a three digit number, identify hundreds place, tens place, ones place
-    s = str(i).zfill(3)
-    hundreds = int(s[0])
-    tens = int(s[1])
-    ones = int(s[2])
-    #Check if the hundreds>0 and if tens+ones>0, spell out 'X hundred and' format
-    if hundreds>0:
-        if (tens+ones)>0:
-            a = 'and '
-        else:
-            a = ''
-        op = str(onesdict[hundreds]) + ' hundred ' + a
-    else:
-        op = ''
-    #if tens are between 20 and 99, spell out 'xty-y' format and stick to output string
-    if 2<=tens:
-        if ones>0:
-            h = '-' + str(onesdict[ones])
-        else:
-            h = ''
-        op+= str(tensdict[tens]) + h
-    #if tens+ones are 10-19, use the teen dictionary for irregular cases
-    elif tens==1:
-        op+= str(teensdict[ones])
-    #only reach this point if tens=0, determine ones spelling and stick to output string
-    elif ones>0:
-        op+= str(onesdict[ones])
+    hundreds = i // 100
+    tens = (i % 100) // 10
+
+    #Get 0-9, or 0-19 if tens place < 2
+    ones = i % 20 if tens < 2 else i % 10
+
+    op = ''
+    #Spell out the hundreds place, with option 'and' if there are also tens or ones
+    if hundreds:
+        op = oneslist[hundreds] + ' hundred '
+        if tens or ones:
+            op += 'and '
+
+    #if tens are between 20 and 99, spell out 'xty-' format and stick to output string
+    if tenslist[tens]:
+        op += tenslist[tens]
+        if ones:
+            op += '-'
+
+    #Add ones place to output string
+    if ones or i==0:
+        op += oneslist[ones]
+
     return op.strip()
 
-def numwords(i):
+def numwords(number):
     """Spell out any integer up to 999 decillion"""
-    if i==0:
-        return 'zero'
+
+    i = abs(int(number))
+
+    # Remember this before we destroy i
+    huns = i % 1000
+
     #define category list containing magnitudes that might need to be included in the spelling
     #list 'hundred' category as empty string because this function will refer to sub1000 function, which already adds 'hundred'
-    catdict = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion']
+    catlist = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion']
     #reading right to left, use sub1000 to spell out every set of three numbers in the sequence
     #attach the appropriate magnitude (cat) to each number depending on where it is in the sequence
     #Creates list of strings formatted like [x hundred, y thousand, z million]. List order is reversed in next step.
-    s = str(abs(i))
-    cat = 0
     wordlist = list()
-    while s!='':
-        if int(s[-3:])!=0:
-            wordlist.append(sub1000(int(s[-3:])) +' ' + catdict[cat])
-        cat+=1
-        s = s[:-3]
+    for cat in catlist:
+        piece = i % 1000
+        i //= 1000
+        if piece or (not wordlist and i==0):
+            wordlist.append(sub1000(piece) + ' ' + cat)
+
+    #Add an 'and' between the first two words if one is not already present,
+    #as in 'one billion and five' instead of 'one billion five', but not
+    #'one billion and one hundred and five'
+    if len(wordlist) > 1 and 0<huns<100: # and not (' and ' in wordlist[0]):
+        wordlist[1] += ' and ' + wordlist[0]
+        wordlist = wordlist[1:]
+
     #print wordlist into a paragraph list in the format 'x, y, z'
-    #if hundreds space is 1-99, add an 'and' as in 'one billion and five' instead of 'one billion, one hundred and five'
     wordlist.reverse()
-    for x in wordlist:
-        ix = wordlist.index(x)
-        huns = int(str(abs(i))[-3:])
-        #start output string with first element
-        if ix==0:
-            op=x
-        #for the last element for numbers between 1-99, add 'and x'
-        elif ix==(len(wordlist)-1) and 0<huns<100:
-            op+=' and '+x
-        #for all other elements, add ', x'
-        else:
-            op+=', '+x
-    if i<0:
+    op = ", ".join(wordlist)
+
+    if number<0:
         op = 'negative '+op
     return op.strip()
 
@@ -99,5 +97,8 @@ def numser(i):
     return op
 
 
-for i in [ 1967, 1993, 1999, 2000, 2001, 2019]:
-    print("{} {:>10}  {}".format(i, numser(i), numwords(i)))
+for i in range(100):
+    print("{:>40} {:>10}  {}".format(i, numser(i), numwords(i)))
+
+for i in [ 1967, 1993, 1999, 2000, 2001, 2019, 200020002000, 123456789, 10**9, 10**35]:
+    print("{:>40} {:>10}  {}".format(i, numser(i) if i < 4000 else "", numwords(i)))
